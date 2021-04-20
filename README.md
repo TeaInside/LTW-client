7c587f08562ff0f2f1ada93622e1fd056309dd02
Aliwoto <aminnimaj@gmail.com>
2021-04-20 01:24:23 -0400

    fixed problems in WotoTool
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Sword Art Online : Last Testament 
 
 
+
 [![Join the chat at https://discord.gg/Nxd9xs4PbN](https://img.shields.io/discord/503992242625576982?color=%25237289DA&label=DOT&logo=discord&logoColor=orange)](https://discord.gg/Nxd9xs4PbN)
--- SAO/Constants/WotoTools.cs
+++ SAO/Constants/WotoTools.cs
@@ -9,67 +9,65 @@ using SAO.Security;
 
 namespace SAO.Constants
 {
-    /// <summary>
-    /// this class, contains main extension methods 
-    /// for sao game.
-    /// </summary>
-    public static class WotoTools
-    {
-        /// <summary>
-        /// get a strong value of this string.
-        /// </summary>
-        /// <param name="value">
-        /// the string.
-        /// </param>
-        /// <returns>
-        /// a strong value of this string.
-        /// </returns>
-        public static StrongString ToStrong(this string value)
-        {
-            return new StrongString(value);
-        }
-        /// <summary>
-        /// calculate the absolute value of this vector2.
-        /// </summary>
-        /// <param name="_v">
-        /// the vector.
-        /// </param>
-        /// <returns>
-        /// a non-negative <see cref="Vector2"/>.
-        /// </returns>
-        public static Vector2 Abs(this Vector2 _v)
-        {
-            float _i1 = _v.X, _i2 = _v.Y;
-            _i1 = MathF.Abs(_i1);
-            _i2 = MathF.Abs(_i2);
-            if (_i1 != _v.X || _i2 != _v.Y)
-            {
-                return new(_i1, _i2);
-            }
-            return _v;
-        }
-        /// <summary>
-        /// calculate the absolute value of this point.
-        /// </summary>
-        /// <param name="_v">
-        /// the point.
-        /// </param>
-        /// <returns>
-        /// a non-negative <see cref="Point"/>.
-        /// </returns>
-        public static Point Abs(this Point _v)
-        {
-            int _i1 = _v.X, _i2 = _v.Y;
-            _i1 = Math.Abs(_i1);
-            _i2 = Math.Abs(_i2);
-            if (_i1 != _v.X || _i2 != _v.Y)
-            {
-                return new(_i1, _i2);
-            }
-            return _v;
-        }
-		
-	
+	/// <summary>
+	/// this class, contains main extension methods 
+	/// for sao game.
+	/// </summary>
+	public static class WotoTools
+	{
+		/// <summary>
+		/// get a strong value of this string.
+		/// </summary>
+		/// <param name="value">
+		/// the string.
+		/// </param>
+		/// <returns>
+		/// a strong value of this string.
+		/// </returns>
+		public static StrongString ToStrong(this string value)
+		{
+			return new StrongString(value);
+		}
+		/// <summary>
+		/// calculate the absolute value of this vector2.
+		/// </summary>
+		/// <param name="_v">
+		/// the vector.
+		/// </param>
+		/// <returns>
+		/// a non-negative <see cref="Vector2"/>.
+		/// </returns>
+		public static Vector2 Abs(this Vector2 _v)
+		{
+			float _i1 = _v.X, _i2 = _v.Y;
+			_i1 = MathF.Abs(_i1);
+			_i2 = MathF.Abs(_i2);
+			if (_i1 != _v.X || _i2 != _v.Y)
+			{
+				return new(_i1, _i2);
+			}
+			return _v;
+		}
+		/// <summary>
+		/// calculate the absolute value of this point.
+		/// </summary>
+		/// <param name="_v">
+		/// the point.
+		/// </param>
+		/// <returns>
+		/// a non-negative <see cref="Point"/>.
+		/// </returns>
+		public static Point Abs(this Point _v)
+		{
+			int _i1 = _v.X, _i2 = _v.Y;
+			_i1 = Math.Abs(_i1);
+			_i2 = Math.Abs(_i2);
+			if (_i1 != _v.X || _i2 != _v.Y)
+			{
+				return new(_i1, _i2);
+			}
+			return _v;
+		}
 	}
 
 }
